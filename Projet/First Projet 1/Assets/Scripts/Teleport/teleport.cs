d6e15146b82bca74bf438758a8d4c25d93a069d6
jetsPepito <36040878+jetsPepito@users.noreply.github.com>
2018-03-10 18:38:15 +0100

    Ajout du tp

---
+++ Projet/First Projet 1/Assets/Scripts/Teleport/teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+	private Vector3 destination;
+	// Update is called once per frame
+	private void OnCollisionEnter(Collision col)
+	{
+		if (this.name == "Portail1")
+		{
+			destination = GameObject.Find(("Portail2")).transform.position;
+		}
+		else
+		{
+			destination = GameObject.Find(("Portail1")).transform.position;
+		}
+
+		col.transform.position = destination - Vector3.forward * 2;
+		col.transform.Rotate(Vector3.up * 180);
+	}
+}
