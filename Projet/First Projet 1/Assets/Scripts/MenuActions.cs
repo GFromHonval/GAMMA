832160dd832b617c802e2763f8d478427519f610
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-10 16:57:09 +0100

    Nouveau Menu

--- Projet/First Projet 1/Assets/Scripts/MenuActions.cs
+++ Projet/First Projet 1/Assets/Scripts/MenuActions.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 
-public class QUIT : MonoBehaviour {
+public class MenuActions : MonoBehaviour {
 
     public void DoQuit()
     {
