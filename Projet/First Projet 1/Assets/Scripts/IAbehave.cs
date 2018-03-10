f177b295d3bffcd076114d2317b64e9d43429816
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-10 12:05:20 +0100

    09/03

---
+++ Projet/First Projet 1/Assets/Scripts/IAbehave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAbehave : Photon.MonoBehaviour
+{
+
+	public Transform Player;
+	public GameObject Head;
+	public Material ColorInnofensif;
+	public Material ColorAttack;
+	private Vector3 StartPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//StartPos = this.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Vector3.Distance(Player.position , this.transform.position) < 10)
+		{
+			Vector3 direction = Player.position - this.transform.position;
+			this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+			//Head.GetComponent<Renderer>().material = ColorAttack;
+		}
+		/*else
+		{
+			this.transform.position = StartPos;
+			Head.GetComponent<Renderer>().material = ColorInnofensif;
+		}*/
+	}
+}
