943dbb18df0366015f5282548ad4e0c4db8757f1
GFromHonval <36164385+GFromHonval@users.noreply.github.com>
2018-03-10 13:57:16 +0100

    new version of design with button

---
+++ Projet/First Projet 1/Assets/_CUSTOM/Photon Unity Networking/UtilityScripts/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+	private float X;
+
+	private float Y;
+
+	private float Z;
+
+	private Vector3 cube;
+	// Use this for initialization
+	void Start ()
+	
+	{ 
+		X = GameObject.Find("FirstPersonCharacter").transform.position.x;
+		Y = GameObject.Find("FirstPersonCharacter").transform.position.y;
+		Z = GameObject.Find("FirstPersonCharacter").transform.position.z;
+		cube = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pers = new Vector3(X,Y,Z);
+		if (pers == cube)
+		{
+			System.Threading.Thread.Sleep(10000);
+			gameObject.transform.Translate(0,-1,0);
+		}
+		
+	}
+}
