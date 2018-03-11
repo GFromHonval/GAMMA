1acd63e586d97f4b0836491e7e0eefa600edf646
jetsPepito <36040878+jetsPepito@users.noreply.github.com>
2018-03-11 14:08:58 +0100

    Revert "Merge branch 'master' of https://github.com/GFromHonval/GAMMA"
    This reverts commit 6ac23233c2d5237b51b55862bf09f7503a1cb1bc, reversing
    changes made to 8d6c15ac16b8878299dbe7d8dc3721508ee1012a.

--- Projet/First Projet 1/Assets/Scripts/Teleport/teleport.cs
+++ Projet/First Projet 1/Assets/Scripts/Teleport/teleport.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
 public class teleport : MonoBehaviour
 {
-	public GameObject VersPortail1;
-	public GameObject VersPortail2;
 	private Vector3 destination;
-	
+	// Update is called once per frame
 	private void OnCollisionEnter(Collision col)
 	{
 		if (this.name == "Portail1")
 		{
-			destination = VersPortail2.transform.position;
+			destination = GameObject.Find(("Portail2")).transform.position;
 		}
 		else
 		{
-			destination = VersPortail1.transform.position;
+			destination = GameObject.Find(("Portail1")).transform.position;
 		}
 
 		col.transform.position = destination - Vector3.forward ;
