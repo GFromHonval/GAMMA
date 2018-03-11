4028222a5dcddb98a18c33fcf93ae58509c40b8d
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-11 11:17:38 +0100

    zxc

--- Projet/First Projet 1/Assets/Timer.cs
+++ Projet/First Projet 1/Assets/Timer.cs
@@ -1,29 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-	private float timer = 180.0f;
-	private bool HasEnded = false;
+    private float timer = 180.0f;
+    private bool HasEnded = false;
 	
 
-	private void Update ()
-	{
-		timer -= Time.deltaTime; //-1 à chaque seconde
-		if (timer <= 0)
-		{
-			timer = 0;
-			GetComponent<PlayerController>().enabled = false;
-			End_Restart.EndGame();
-		}
+    private void Update ()
+    {
+        timer -= Time.deltaTime; //-1 à chaque seconde
+        if (timer <= 0)
+        {
+            timer = 0;
+            GetComponent<PlayerController>().enabled = false;
+            End_Restart.EndGame();
+        }
 
-	}
+    }
 
-	private void OnGUI()
-	{
-		GUI.Box(new Rect(10, 10, 40, 20), timer.ToString("0"));
-	}
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 40, 20), timer.ToString("0"));
+    }
 
 }
