f117c9edddb5ce78095d41e225d54d203ceca9e0
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-12 01:05:11 +0100

    respawn

--- Projet/First Projet 1/Assets/Scripts/RespawnQuandOnTombe.cs
+++ Projet/First Projet 1/Assets/Scripts/RespawnQuandOnTombe.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RespawnQuandOnTombe : MonoBehaviour
 {
 
-	[SerializeField] private Transform player;
+	private GameObject[] players;
+	[SerializeField] private GameObject LePlusbas;
 	[SerializeField] private Transform respawnPoint;
 
-	private void OnTriggerEnter(Collider other)
+	void FixedUpdate()
 	{
-		player.transform.position = respawnPoint.transform.position;
+		players = GameObject.FindGameObjectsWithTag("Player");
+		Debug.Log("seconde");
+		foreach (GameObject player in players)
+		{
+			if (player.transform.position.y < LePlusbas.transform.position.y)
+			{
+				Debug.Log("tu tombes fdp");
+				player.transform.position = respawnPoint.transform.position;
+			}
+
+		}
 	}
 }
