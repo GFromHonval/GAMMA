fae5ddcf14adcdb2d00e081c5e68d6f5b9e8add3
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-13 11:28:53 +0100

    bite

--- Projet/First Projet 1/Assets/Scripts/RespawnQuandOnTombe.cs
+++ Projet/First Projet 1/Assets/Scripts/RespawnQuandOnTombe.cs
@@ -7,41 +7,15 @@ using UnityEngine.AI;
 public class RespawnQuandOnTombe : MonoBehaviour
 {
 
-	private GameObject[] players;
-	[SerializeField] private GameObject LePlusbas;
-	[SerializeField] private Transform respawnPoint;
-	[SerializeField] private GameObject GameOver;
-	[SerializeField] private float Life;
-	[SerializeField] private float Damage;
+	public GameObject LePlusBas;
+	public Transform RespawnPoint;
+	public GameObject GameOver;
+	public float LifeInThisLevel;
+	public float DamageFallOfThisLevel;
 	
 	private void Start()
 	{
 		GameOver.SetActive(false);
 	}
 
-	void FixedUpdate()
-	{
-		
-		players = GameObject.FindGameObjectsWithTag("Player");
-		Debug.Log("seconde");
-		foreach (GameObject player in players)
-		{
-			if (player.transform.position.y < LePlusbas.transform.position.y)
-			{
-				if (Life <= Damage)
-				{
-					GameOver.SetActive(true);
-					player.transform.position = respawnPoint.transform.position;
-					
-				}
-				else
-				{
-					player.transform.position = respawnPoint.transform.position;
-					Life -= Damage;
-				}
-				
-			}
-
-		}
-	}
 }
--- Projet/First Projet 1/Assets/Scripts/RotationPlayer.cs
+++ Projet/First Projet 1/Assets/Scripts/RotationPlayer.cs
@@ -1,33 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 
-public class RotationPlayer : MonoBehaviour {
+public class RotationPlayer : MonoBehaviour
+{
+	private float Life;
+    private float Damage;
+	private GameObject GameL;
+	private GameObject LePlusB;
+	private Transform RespawnP;
+	private GameObject GameO;
+	
 	public float moveSpeed = 10f;
 	public float turnSpeed = 50f;
 	public float jumpPower;
 	private bool IsJumping = false;
-   
+
+	void Start()
+	{
+		GameL = GameObject.Find("GameLogic");
+		RespawnQuandOnTombe RespawnScript = GameL.GetComponent<RespawnQuandOnTombe>();
+		LePlusB = RespawnScript.LePlusBas;
+		RespawnP = RespawnScript.RespawnPoint;
+		GameO = RespawnScript.GameOver;
+		Life = RespawnScript.LifeInThisLevel;
+		Damage = RespawnScript.DamageFallOfThisLevel;
+	}
+
+
 	void Update () {
 		
-		if(Input.GetKey(KeyCode.UpArrow))
-			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.DownArrow))
-			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.LeftArrow))
-			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-        
-		if(Input.GetKey(KeyCode.RightArrow))
-			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-
-		if (Input.GetKey(KeyCode.Space) && !IsJumping)
+		if (transform.position.y < LePlusB.transform.position.y)
 		{
-			//IsJumping = true;	
-			transform.Translate(Vector3.up* jumpPower * Time.deltaTime);
+			if (Life <= Damage)
+			{
+				GameO.SetActive(true);
+				transform.position = RespawnP.transform.position;
+				transform.rotation = RespawnP.transform.rotation;
+				GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
+				Life = 0;
+			}
+			else
+			{
+				transform.position = RespawnP.transform.position;
+				transform.rotation = RespawnP.transform.rotation;
+				GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
+				Life -= Damage;
+			}
 		}
+
+		if (Life > 0)
+		{
+			if (Input.GetKey(KeyCode.UpArrow))
+				transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+			if (Input.GetKey(KeyCode.DownArrow))
+				transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+			if (Input.GetKey(KeyCode.LeftArrow))
+				transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+			if (Input.GetKey(KeyCode.RightArrow))
+				transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+			if (Input.GetKey(KeyCode.Space) && !IsJumping)
+			{
+				//IsJumping = true;	
+				transform.Translate(Vector3.up * jumpPower * Time.deltaTime);
+			}
+		}
+		
 	}
 
 }
