29bfc7484425cec996c7e3032f83b3b32d3f88cd
jetsPepito <louis.le-gatt@epita.fr>
2018-03-21 22:23:28 +0100

    levle music

--- Projet/First Projet 1/Assets/Scripts/ActivatorScript.cs
+++ Projet/First Projet 1/Assets/Scripts/ActivatorScript.cs
@@ -7,18 +7,31 @@ public class ActivatorScript : MonoBehaviour
 
 	public KeyCode key;
 	private bool active = false;
+	private SpriteRenderer sr;
+	Color old ;
+	public float sec;
 
 	private GameObject note;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		old = sr.color;
 	}
-	
+
+	private void Awake()
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(key))
+			StartCoroutine(Pressed());
 		if (Input.GetKeyDown(key) && active)
 		{
 			Destroy(note);
+			AddScore();
+			active = false;
 		}
 	}
 
@@ -35,5 +48,18 @@ public class ActivatorScript : MonoBehaviour
 	private void OnTriggerExit2D(Collider2D col)
 	{
 		active = false;
+	}
+
+	void AddScore()
+	{
+		PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
+	}
+
+	IEnumerator Pressed()
+	{
+		
+		sr.color = new Color(0,0,0);
+		yield return new WaitForSeconds(sec);
+		sr.color = old;
 	}
 }
