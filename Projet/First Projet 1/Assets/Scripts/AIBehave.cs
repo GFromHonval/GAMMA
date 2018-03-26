05761cd7b88504a93156cf66a554fde3fe28e14e
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-03-26 18:34:23 +0200

    fghj,

--- Projet/First Projet 1/Assets/Scripts/AIBehave.cs
+++ Projet/First Projet 1/Assets/Scripts/AIBehave.cs
@@ -10,7 +10,7 @@ public class AIBehave : Photon.MonoBehaviour {
 	public GameObject Head;
 	public Material ColorInnofensif;
 	public Material ColorAttack;
-	private Vector3 StartPos;
+	private Transform StartPos;
 	public GameObject[] waypoints;
 	private int currentWP;
 	private float rotSpeed = 10f;
@@ -21,7 +21,7 @@ public class AIBehave : Photon.MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		StartPos = this.transform.position;
+		StartPos = this.transform;
 	}
 	
 	// Update is called once per frame
@@ -32,22 +32,7 @@ public class AIBehave : Photon.MonoBehaviour {
 		{
 			Vector3 direction = P.transform.position - transform.position;
 			direction.y = 0;
-			if (waypoints.Length > 0)
-			{
-				if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
-				{
-					currentWP++;
-					if (currentWP >= waypoints.Length)
-					{
-						currentWP = 0;
-					}
-				}
-	
-				direction = waypoints[currentWP].transform.position - transform.position;
-				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),rotSpeed*Time.deltaTime);
-				transform.Translate(0,0,Time.deltaTime * speed);
-			}
-
+			
 			if (Vector3.Distance(P.transform.position, this.transform.position) < DistanceDeDetection)
 			{
 				this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
@@ -55,9 +40,32 @@ public class AIBehave : Photon.MonoBehaviour {
 			}
 			else
 			{
-				this.transform.position = StartPos;
-				Head.GetComponent<Renderer>().material = ColorInnofensif;
+				if (waypoints.Length > 0)
+				{
+					Head.GetComponent<Renderer>().material = ColorInnofensif;
+					if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+					{
+						currentWP++;
+						if (currentWP >= waypoints.Length)
+						{
+							currentWP = 0;
+						}
+					}
+		
+					direction = waypoints[currentWP].transform.position - transform.position;
+					transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),rotSpeed*Time.deltaTime);
+					transform.Translate(0,0,Time.deltaTime * speed);
+				}
+				else
+				{
+					transform.position = StartPos.position;
+					transform.rotation = StartPos.rotation;
+					Head.GetComponent<Renderer>().material = ColorInnofensif;
+				}
 			}
+			
+			
+
 		}
 	}
 }
---
+++ Projet/First Projet 1/Assets/Scripts/BoutonEtPlateforme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonEtPlateforme : MonoBehaviour
+{
+
+	public Transform BassePosition;
+	public Transform HautePosition;
+	public GameObject Plateforme;
+	private Vector3 MouvementDirection;
+		
+	// Update is called once per frame
+	private void OnCollisionEnter(Collision other)
+	{
+		this.transform.position = Vector3.MoveTowards(HautePosition.position, BassePosition.position, Time.deltaTime);
+	}
+}
