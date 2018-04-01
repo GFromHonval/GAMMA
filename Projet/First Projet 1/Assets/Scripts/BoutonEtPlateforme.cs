0eae638c00e6ec146c7ba02aef03e6225e5c1a02
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-04-01 16:38:08 +0200

    bouton + plateforme dans sceneprincipale

--- Projet/First Projet 1/Assets/Scripts/BoutonEtPlateforme.cs
+++ Projet/First Projet 1/Assets/Scripts/BoutonEtPlateforme.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Analytics;
 using Debug = UnityEngine.Debug;
 
 public class BoutonEtPlateforme : MonoBehaviour
@@ -12,47 +16,95 @@ public class BoutonEtPlateforme : MonoBehaviour
 
 	public Transform BassePosition;
 	public GameObject Plateforme;
+	private Vector3 StartPlateforme;
+	public GameObject EndPlateforme;
 	private Vector3 MouvementDirection;
 	private Vector3 BassePose;
 	private Vector3 HautePose;
 	private bool IsPressing;
-	private bool IsInArea;
-	private Vector3 WherePlayer;
-	private string PlayerTag;
+	private string PlayerTag = "";
+	private Vector4 Area;
+	private string PlateformeState;
 	// Update is called once per frame
 
 	private void Start()
 	{
 		BassePose = new Vector3(transform.position.x, BassePosition.position.y, transform.position.z);
 		HautePose = transform.position;
+		Area.x = transform.position.z - GetComponent<BoxCollider>().size.z * transform.localScale.z /2;
+		Area.y = transform.position.z + GetComponent<BoxCollider>().size.z * transform.localScale.z /2;
+		Area.z = transform.position.x - GetComponent<BoxCollider>().size.x * transform.localScale.x /2;
+		Area.w = transform.position.x + GetComponent<BoxCollider>().size.x * transform.localScale.x /2;
+		StartPlateforme = new Vector3 (Plateforme.transform.position.x, Plateforme.transform.position.y, Plateforme.transform.position.z);
+		PlateformeState = "Monte";
 	}
 
 	private void Update()
 	{
-		Debug.Log(IsPressing);
-		if (IsPressing && transform.position.y > BassePose.y)
+		if (IsPressing)
+			ToDo();
+		if (IsPressing && transform.position.y >= BassePose.y)
 		{
 			transform.position = Vector3.MoveTowards(transform.position, BassePose, Time.deltaTime);
+			IsInArea();	
 		}
-		if (GameObject.FindGameObjectWithTag(PlayerTag).transform.position.y < WherePlayer.y)
+		if (!IsPressing && transform.position.y < HautePose.y)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, HautePose, Time.deltaTime);
+			PlayerTag = "";
+		}
+	}
+
+	private void ToDo()
+	{
+		Debug.Log(PlateformeState);
+		if (PlateformeState == "Monte")
+		{
+			Plateforme.transform.position = Vector3.MoveTowards(Plateforme.transform.position, EndPlateforme.transform.position, Time.deltaTime);
+			if (Plateforme.transform.position.y >= EndPlateforme.transform.position.y)
+			{
+				PlateformeState = "Descend";
+			}
+		}
+		else
+		{
+			if (PlateformeState == "Descend")
+			{
+				Plateforme.transform.position = Vector3.MoveTowards(Plateforme.transform.position, StartPlateforme, Time.deltaTime);
+				if (Plateforme.transform.position.y <= StartPlateforme.y)
+				{
+					PlateformeState = "Monte";
+				}
+			}
+		}
+	}
+	
+	private void IsInArea()
+	{
+		if (PlayerTag != "" && GameObject.FindGameObjectWithTag(PlayerTag).transform.position.z < Area.x
+		    || GameObject.FindGameObjectWithTag(PlayerTag).transform.position.z > Area.y
+		    || GameObject.FindGameObjectWithTag(PlayerTag).transform.position.x < Area.z
+		    || GameObject.FindGameObjectWithTag(PlayerTag).transform.position.x > Area.w)
 		{
 			IsPressing = false;
 		}
-		if (!IsInArea && transform.position.y < HautePose.y)
+		else
 		{
-			transform.position = Vector3.MoveTowards(transform.position, HautePose, Time.deltaTime);
+			IsPressing = true;
 		}
 	}
 	
 	private void OnCollisionEnter(Collision other)
 	{
-		float high = GetComponent<BoxCollider>().size.y * transform.localScale.y / 2;
+		//FirstTry
+		/*float high = GetComponent<BoxCollider>().size.y * transform.localScale.y / 2;
 		Vector3 contact = other.contacts[0].point;
-		WherePlayer = other.transform.position;
 		PlayerTag = other.gameObject.tag;
 		if (contact.y >= transform.position.y + high)
 		{
 			IsPressing = true;
-		}
+		}*/
+		PlayerTag = other.gameObject.tag;
+		IsInArea();
 	}
 }
