d6db58bebd15ed139cd6f032ef5170c663e95a94
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-04-09 18:20:36 +0200

    cfg

--- Projet/First Projet 1/Assets/Scripts/TutoPhoton/PhotonNetworkManager.cs
+++ Projet/First Projet 1/Assets/Scripts/TutoPhoton/PhotonNetworkManager.cs
@@ -10,6 +10,7 @@ public class PhotonNetworkManager : Photon.MonoBehaviour
 	[SerializeField] private GameObject Player;
 	[SerializeField] private Transform SpawnPoint;
 	[SerializeField] private GameObject LobbyCamera;
+	[SerializeField] private List<string> Joueurs;
 	
 	// Use this for initialization
 	void Start ()
@@ -20,6 +21,17 @@ public class PhotonNetworkManager : Photon.MonoBehaviour
 	private void OnJoinedLobby()
 	{
 		Debug.Log("You are now in the lobby");
+		if (Joueurs.Count == 0)
+		{
+			Joueurs.Add("MainPlayer");
+			//differencier menu des autres scenes
+			//trouver un moyen de garder les choix des joueurs en chargeant les scenes
+			
+		}
+		else
+		{
+			
+		}
 		
 		//RoomOptions pour changer les options de la room
 		//Join room if it exist or create one
