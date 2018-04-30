b9fc526b101dde6c8d2e79b5b81f11ec9b98c38b
GFromHonval <36164385+GFromHonval@users.noreply.github.com>
2018-05-01 00:35:03 +0200

    website f

---
+++ Website/Gamma Game/Avancement du projet(var).js
@@ -0,0 +1,1 @@
+var OEConfWEVideo = {"WE20325a42e4":{"ExportValues":"Ce lecteur Flash fonctionnera après <b>LA MISE EN LIGNE du site</b><br/><b><u>(les vidéos locales</u></b> ne se chargent pas Flash)##ERREUR: PAS DE VIDEO SOUTENUE","Links":[{"Target":null,"Links":{"Items":{"DEFAULT":"Files/Video/Light_Bulb_4___13s___4k_res.mp4"}}},null,null]}}
