06afb62e0b190fd1fac5d2a85ebb5fb43ee4262d
GFromHonval <36164385+GFromHonval@users.noreply.github.com>
2018-05-03 11:25:33 +0200

    webstie v2
    top

---
+++ Website/Gamma Game_Upload (3)/Telecharger rapport(var).js
@@ -0,0 +1,1 @@
+var OEConfWEVideo = {"WEceb44064d7":{"ExportValues":"Ce lecteur Flash fonctionnera après <b>LA MISE EN LIGNE du site</b><br/><b><u>(les vidéos locales</u></b> ne se chargent pas Flash)##ERREUR: PAS DE VIDEO SOUTENUE","Links":[{"Target":null,"Links":{"Items":{"DEFAULT":"Files/Video/Hello-World.mp4"}}},null,null]}}
