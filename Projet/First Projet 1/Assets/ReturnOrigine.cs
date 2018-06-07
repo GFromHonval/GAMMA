207bf9f991325f708b7f5068f5f18ecc67a3225e
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-06-07 13:01:32 +0200

    scripts lvl1

---
+++ Projet/First Projet 1/Assets/ReturnOrigine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ReturnOrigine : MonoBehaviour
+{
+	public float Vitesse;
+	public GameObject Origine;
+
+	private Vector3 Origin;
+	private Vector3 Old;
+	private bool Return;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Origin = Origine.transform.position;
+		Old = Origin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!Return && Old - transform.position == Vector3.zero)
+		{
+			print("return");
+			Return = true;
+		}
+		else
+		{
+			Old = transform.position;
+		}
+
+		if (Return)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, Origin, Time.deltaTime * Vitesse);
+			if (transform.position == Origin)
+				Return = false;
+		}
+	}
+}
