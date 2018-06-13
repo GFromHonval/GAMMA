a6ccc2c47fe57b0a4d40bdead145c746d1773db9
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-06-13 17:16:23 +0200

    ujhyg

--- Projet/First Projet 1/Assets/Scripts/Timer.cs
+++ Projet/First Projet 1/Assets/Scripts/Timer.cs
@@ -27,7 +27,9 @@ public class Timer : MonoBehaviour
         {
             Time = 0;
             if (SceneManager.GetActiveScene().name != "Menu principal" &&
-                SceneManager.GetActiveScene().name != "Menu without logic")
+                SceneManager.GetActiveScene().name != "Menu without logic" && SceneManager.GetActiveScene().name != "Niveau musical 1" 
+                && SceneManager.GetActiveScene().name != "Niveau musical 2" && SceneManager.GetActiveScene().name != "Niveau musical 3"
+                && SceneManager.GetActiveScene().name != "Niveau musical 4")
             {
                 GameOver.enabled = true;
                 if (GameObject.FindGameObjectWithTag("PlayerBoy") != null)
