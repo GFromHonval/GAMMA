05f6c9b6c411fb449b18dd3ccfca09e79128430c
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-06-13 15:04:59 +0200

    volume

---
+++ Projet/First Projet 1/Assets/VolumeChangerEscape.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VolumeChangerEscape : MonoBehaviour {
+
+
+	void Update () {
+
+		if (SceneManager.GetActiveScene().name != "Menu principal" &&
+		    SceneManager.GetActiveScene().name != "Menu without logic")
+		{
+			
+			AudioSource Musique;
+			Musique = GameObject.Find("GameLogic").GetComponentInChildren<AudioSource>();
+			Musique.volume = GetComponent<Scrollbar>().value * 0.2f;
+		}
+	}
+}
