a9e8d6d61f11d19b78c8f7aedb2f5defde37e321
Kazoryah <35991545+Kazoryah@users.noreply.github.com>
2018-06-13 18:20:52 +0200

    gfrds

---
+++ Projet/First Projet 1/Assets/DesMusicGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Interfaces;
+using UnityEngine;
+using UnityEngine.Collections;
+using UnityEngine.SceneManagement;
+
+public class DesMusicGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (GameObject.Find("GameLogic"))
+			GameObject.Find("GameLogic").GetComponentInChildren<AudioSource>().mute = true;
+
+		SceneManager.activeSceneChanged += UnmuteMusic;
+	}
+
+	private void UnmuteMusic(Scene prev, Scene next)
+	{
+		if (next.name == "Menu without logic")
+			GameObject.Find("GameLogic").GetComponentInChildren<AudioSource>().mute = false;
+	}
+}
---
+++ Projet/First Projet 1/Assets/MuteMusicGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MuteMusicGame : MonoBehaviour {
+
+	// Use this for initialization
+	// Use this for initialization
+		void Start ()
+		{
+			if (GameObject.Find("GameLogic"))
+				GameObject.Find("GameLogic").GetComponentInChildren<AudioSource>().mute = true;
+
+			SceneManager.activeSceneChanged += UnmuteMusic;
+		}
+
+		private void UnmuteMusic(Scene prev, Scene next)
+		{
+			if (next.name == "Menu without logic")
+				GameObject.Find("GameLogic").GetComponentInChildren<AudioSource>().mute = false;
+		}
+}
